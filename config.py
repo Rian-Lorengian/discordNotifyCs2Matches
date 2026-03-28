2ddf83a3bde5a491141866793b7838dd92f79a0b
Rian-Lorengian <rianlorengian@gmail.com>
2026-03-28 19:06:56 -0300

    ajuste de dependencias

--- config.py
+++ config.py
@@ -20,8 +20,8 @@ BEARER_TOKEN = _exigir("BEARER_TOKEN_API")
 WEBHOOK_LOGS = _exigir("WEBHOOK_URL_3")
 
 CONFIG_WEBHOOKS = [
-    WebhookConfig(url=_exigir("WEBHOOK_URL_1"), mencoes=["Teste"]),
-    # WebhookConfig(url=_exigir("WEBHOOK_URL_2"), mencoes=["<@&796530374519160872>"]),
+    WebhookConfig(url=_exigir("WEBHOOK_URL_1"), mencoes=["Aviso"]),
+    WebhookConfig(url=_exigir("WEBHOOK_URL_2"), mencoes=["<@&796530374519160872>"]),
 ]
 
 # Times monitorados
